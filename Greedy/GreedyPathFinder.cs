84a216060642b6c995d39eaf5505e1ce2cc8fd20
pfmaria13 <105740928+pfmaria13@users.noreply.github.com>
2022-06-17 00:50:16 +0500

    Add files via upload

---
+++ Greedy/GreedyPathFinder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using Greedy.Architecture;
+using Greedy.Architecture.Drawing;
+
+namespace Greedy
+{
+    public class GreedyPathFinder : IPathFinder
+    {
+        public List<Point> FindPathToCompleteGoal(State state)
+        {
+            var chests = state.Chests.ToHashSet();
+            var energy = 0;
+            var goal = 0;
+            var pathFinder = new DijkstraPathFinder();
+            var ways = new List<Point>();
+            var start = state.Position;
+            while (goal < state.Goal && energy < state.Energy)
+            {
+                var pathsToChests = pathFinder.GetPathsByDijkstra(state, start, chests);
+                var shortWay = pathsToChests.FirstOrDefault();
+                if (shortWay == null)
+                    return new List<Point>();
+                goal++;
+                energy += shortWay.Cost;
+                ways.AddRange(shortWay.Path.Skip(1));
+                start = shortWay.End;
+                chests.Remove(start);
+            }
+            return energy > state.Energy || goal < state.Goal ? new List<Point>() : ways;
+        }
+    }
+}
---
+++ Greedy/NotGreedyPathFinder.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using Greedy.Architecture;
+using Greedy.Architecture.Drawing;
+
+namespace Greedy
+{
+	public class NotGreedyPathFinder : IPathFinder
+	{
+		private List<Point> bestPath = new List<Point>();
+		private int bestCountOfChests;
+		private int countOfChests;
+
+		private void FindPath(State state, Point startPoint, List<Point> remainingСhests,
+							   List<Point> passedPath, int energy, int countFoundChests)
+		{
+			if (bestCountOfChests < countFoundChests)
+			{
+				bestPath = passedPath;
+				bestCountOfChests = countFoundChests;
+			}
+			if (bestCountOfChests == countOfChests || countFoundChests == countOfChests) return;
+			var finder = new DijkstraPathFinder();
+			var paths = finder.GetPathsByDijkstra(state, startPoint, remainingСhests)
+				.Where(e => energy + e.Cost <= state.Energy);
+			foreach (var path in paths)
+			{
+				remainingСhests.Remove(path.End);
+				FindPath(state, path.End, remainingСhests, passedPath.Concat(path.Path.Skip(1)).ToList(),
+						  path.Cost + energy, countFoundChests + 1);
+				remainingСhests.Add(path.End);
+			}
+		}
+
+		public List<Point> FindPathToCompleteGoal(State state)
+		{
+			countOfChests = state.Chests.Count;
+			FindPath(state, state.Position, state.Chests, new List<Point>(), 0, 0);
+			return bestPath;
+		}
+	}
+}
