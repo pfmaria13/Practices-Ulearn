5f01f7f3042399e8bcb3cb3c40164156e7f7361a
pfmaria13 <105740928+pfmaria13@users.noreply.github.com>
2022-06-17 00:20:08 +0500

    Add files via upload

---
+++ Limited Size Stack/LimitedSizeStack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApplication
+{
+    public class LimitedSizeStack<T>
+    {
+        private readonly LinkedList<T> stack = new LinkedList<T>();
+        private readonly int limitation;
+
+        public LimitedSizeStack(int limit)
+        {
+            limitation = limit;
+        }
+
+        public void Push(T item)
+        {
+            if (limitation != 0)
+            {
+                if (stack.Count == limitation)
+                    stack.RemoveFirst();
+                stack.AddLast(item);
+            }
+        }
+
+        public T Pop()
+        {
+            var result = stack.Last.Value;
+            stack.RemoveLast();
+            return result;
+        }
+
+        public int Count => stack.Count;
+    }
+}
